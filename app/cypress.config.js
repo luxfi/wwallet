76644a8b3a39e21f8b5c386aff8911b947a53056
Jason <jason4271735@gmail.com>
2024-07-05 18:01:44 +0800

    update main ci

---
+++ app/cypress.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+    chromeWebSecurity: false,
+    fixturesFolder: false,
+
+    e2e: {
+        baseUrl: 'http://localhost:5000', // 修改为你实际的应用地址
+    },
+})
